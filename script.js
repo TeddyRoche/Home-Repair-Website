7fb80a26ae382f073dfa23474f7dee669c429fd1
TeddyRoche <theodore.r.roche@lions.enc.edu>
2023-08-21 10:09:13 -0400

    updates

--- script.js
+++ script.js
@@ -37,13 +37,53 @@ document.addEventListener("DOMContentLoaded", function() {
 
 //Lists-------------------------------------------------------------------------------------
 const sectionNames = [
-    "Home", "Kitchen", "Living Room", "Office", 
-    "Attic", "Bathroom", "Dining Room", 
-    "Bedroom", "Basement", "Laundry Room", "About"
+    "Home", "Electric", "Plumbing", "Walls", 
+    "Floor & Ceiling", "Heat & AC",  "About"
 ];
 
+const gridItemDestinations = {
+    electric: ['electric1.html', 'electric2.html', 'electric3.html', 'electric4.html', 'electric5.html', 'electric6.html'],
+    plumbing: ['plumbing1.html', 'plumbing2.html', 'plumbing3.html', 'plumbing4.html', 'plumbing5.html', 'plumbing6.html'],
+    walls: ['walls1.html', 'walls2.html', 'walls3.html', 'walls4.html', 'walls5.html', 'walls6.html'],
+    'floor-&-ceiling': ['floor-&-ceiling1.html', 'floor-&-ceiling2.html', 'floor-&-ceiling3.html', 'floor-&-ceiling4.html', 'floor-&-ceiling5.html', 'floor-&-ceiling6.html'],
+    'heat-&-ac': ['heat-&-ac1.html', 'heat-&-ac2.html', 'heat-&-ac3.html', 'heat-&-ac4.html', 'heat-&-ac5.html', 'heat-&-ac6.html'],
 
-const imageFormats = ['jpg', 'jpeg', 'png', 'gif']; // Add more formats if needed
+};
+
+function createAndNavigateToHTMLPage(sectionName, itemNumber) {
+    const content = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>${sectionName} Item ${itemNumber}</title>
+        </head>
+        <body>
+            <h1>${sectionName} Item ${itemNumber}</h1>
+            <!-- Add your content here -->
+        </body>
+        </html>
+    `;
+
+    // Create a Blob from the HTML content
+    const blob = new Blob([content], { type: 'text/html' });
+
+    // Generate a URL for the Blob
+    const url = URL.createObjectURL(blob);
+
+    // Create an anchor link to navigate to the new page
+    const link = document.createElement('a');
+    link.href = url;
+
+    // Trigger a click event to navigate to the new page
+    link.click();
+
+    // Revoke the URL to free up resources (optional)
+    URL.revokeObjectURL(url);
+}
+
+
+
+const imageFormats = ['jpg', 'jpeg', 'png', 'gif', 'webp']; // Add more formats if needed
 
 const styleSheet = document.styleSheets[0]; // Get the first stylesheet
 const sectionsContainer = document.querySelector('.sections');
@@ -53,10 +93,6 @@ const sectionsContainer = document.querySelector('.sections');
 sectionNames.forEach(sectionName => {
     if (sectionName !== "Home" && sectionName !== "About") {
     const sectionClassName = sectionName.replace(/\s+/g, '-').toLowerCase();
-    
-
-    
-
     const sectionDiv = document.createElement('div');
     sectionDiv.classList.add(sectionClassName);
     sectionDiv.classList.add('section'); // Add a class to the section
@@ -80,7 +116,6 @@ sectionNames.forEach(sectionName => {
     const sectionLink = document.createElement('a');
     sectionLink.href = `#${sectionDiv.id}`;
     sectionLink.textContent = sectionName;
-
     const sectionGrid = document.createElement('section');
     sectionGrid.classList.add('grid-con');
     
@@ -93,6 +128,11 @@ for (let i = 1; i <= (sectionName === 'Laundry Room' ? 3 : 6); i++) {
     gridItem.style.borderWidth = '2px';
     gridItem.style.borderStyle = 'solid';
 
+
+    gridItem.addEventListener('click', () => {
+        createAndNavigateToHTMLPage(sectionName, i);
+    });
+    
 
         //Grid Images--------------------------------------------------------------------
         let backgroundImageUrl;
@@ -126,15 +166,15 @@ for (let i = 1; i <= (sectionName === 'Laundry Room' ? 3 : 6); i++) {
 // Get all section elements
 const sectionElements = document.querySelectorAll('.section');
 
-// Set the "Kitchen" section as active when the page loads
+// Set the "Electric" section as active when the page loads
 document.addEventListener("DOMContentLoaded", function () {
-    // Activate the "Kitchen" section
-    const kitchenSection = document.getElementById('kitchen');
-    kitchenSection.classList.add('active-section');
+    // Activate the "Electric" section
+    const firstSection = document.getElementById('electric');
+    firstSection.classList.add('active-section');
 
     // Deactivate all other sections (except "Home")
     sectionElements.forEach(function (section) {
-        if (section !== kitchenSection && section.id !== 'home' && section.id !== 'about') {
+        if (section !== firstSection && section.id !== 'home' && section.id !== 'about') {
             section.classList.add('hidden-section');
         }
     });
